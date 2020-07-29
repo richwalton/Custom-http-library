186a41e3b7fe48545fe084cce444638b9cf27144
Rich Walton <sbtsun@yahoo.com>
2020-07-29 14:44:20 -0700

    Added put and delete prototypes

--- easyhttp3.js
+++ easyhttp3.js
@@ -1,3 +1,4 @@
+//constructor function
 function easyHTTP() {
     this.http = new XMLHttpRequest();
 }
@@ -18,7 +19,43 @@ easyHTTP.prototype.get = function(url, callback) {
 }
 
 // Make an HTTP POST request
+easyHTTP.prototype.post = function(url, data, callback) {
+    this.http.open('POST', url, true);
+    this.http.setRequestHeader('Content-type', 'application/json');
+
+    let self = this;
+    this.http.onload = function() {
+    callback(null, self.http.responseText);
+    } 
+
+    this.http.send(JSON.stringify(data));
+}
 
 // Make an HTTP PUT request
+easyHTTP.prototype.put = function(url, data, callback) {
+    this.http.open('PUT', url, true);
+    this.http.setRequestHeader('Content-type', 'application/json');
+
+    let self = this;
+    this.http.onload = function() {
+    callback(null, self.http.responseText);
+    } 
+
+    this.http.send(JSON.stringify(data));
+}
+
 
 // Make an HTTP DELETE request
+easyHTTP.prototype.delete = function(url, callback) {
+    this.http.open('DELETE', url, true);
+    //Lexical 'this' using 'self' varible in es5 otherwise use arrow function in es6
+    let self = this;
+    this.http.onload = function() {
+        if(self.http.status === 200) {
+            callback(null, 'Post Deleted');
+        } else {
+            callback('Error:' + self.http.status);
+        }
+    }
+    this.http.send();
+}
